a0c764d5e60cc94eab51def0caed8a5f03d871b4
Benjamin Mentink <300079207@fvtc.edu>
2023-08-31 13:39:37 -0500

    Added Hobbies Modified CSS

---
+++ BJM.School.UI/Pages/Hobbies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BJM.School.UI.Pages
+{
+    public class HobbiesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
